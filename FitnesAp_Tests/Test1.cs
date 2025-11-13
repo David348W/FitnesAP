4d49f144e3d8a3e62569514b81190c7c0eb5cffe
David M <majcendado@gmail.com>
2025-11-13 12:49:18 +0100

    TESTI

---
+++ FitnesAp_Tests/Test1.cs
@@ -0,0 +1,18 @@
+﻿namespace FitnesAp_Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+
+        public void TestMethod1()
+        {
+            Assert.IsFalse(false);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
