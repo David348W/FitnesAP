d24800fc1ec6a7b42abc0dacb28042e3a767a3a1
David M <majcendado@gmail.com>
2025-12-06 19:15:08 +0100

    Dodajanje Profila

---
+++ FitnesAP/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FitnesAP.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; } = "User";
+
+        public string? Ime { get; set; }      
+        public string? Priimek { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ FitnesAP/data/UserService.cs
@@ -0,0 +1,90 @@
+﻿using FitnesAP.Models;
+using System.Text.Json;
+
+namespace FitnesAP.Data
+{
+    public class UserService
+    {
+        
+        private readonly string _path = "data/Users.json";
+
+        public List<User> GetUsers()
+        {
+            if (!File.Exists(_path)) return new List<User>();
+
+            var json = File.ReadAllText(_path);
+            return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+        }
+
+        public void SaveUsers(List<User> users)
+        {
+            var json = JsonSerializer.Serialize(users, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(_path, json);
+        }
+
+        public bool CheckLogin(string username, string password)
+        {
+            return GetUsers().Any(u => u.Username == username && u.Password == password);
+        }
+
+        public bool Register(string username, string password)
+        {
+            var users = GetUsers();
+
+            if (users.Any(u => u.Username == username))
+                return false;
+
+            var newUser = new User
+            {
+                Id = users.Count > 0 ? users.Max(u => u.Id) + 1 : 1, 
+                Username = username,
+                Password = password,
+               
+                Ime = "",
+                Priimek = ""
+            };
+
+            users.Add(newUser);
+            SaveUsers(users);
+
+            return true;
+        }
+
+        
+        public User? GetUserByUsername(string username)
+        {
+            var users = GetUsers();
+            return users.FirstOrDefault(u => u.Username == username);
+        }
+
+        
+        public void UpdateUser(User userToUpdate)
+        {
+            var users = GetUsers();
+
+            
+            var existingUser = users.FirstOrDefault(u => u.Id == userToUpdate.Id);
+
+            if (existingUser != null)
+            {
+               
+                existingUser.Ime = userToUpdate.Ime;
+                existingUser.Priimek = userToUpdate.Priimek;
+                existingUser.Email = userToUpdate.Email;
+
+                
+                if (!string.IsNullOrEmpty(userToUpdate.Password))
+                {
+                    existingUser.Password = userToUpdate.Password;
+                }
+
+                
+                SaveUsers(users);
+            }
+        }
+    }
+}
